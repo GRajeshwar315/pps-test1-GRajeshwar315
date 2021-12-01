94cb189b0a9c866a981eaa0c22bf300ead39d8d9
GRajeshwar315 <94883446+GRajeshwar315@users.noreply.github.com>
2021-12-01 10:16:17 +0000

    my name

--- p0final.c
+++ p0final.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main()
 {
-  printf("my name is rajesh\n");
-    return 0;
+  printf("rajesh\n");
+  return 0;
 }
-
---
+++ p1final.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter two number\n");
+  scanf("%d%d" ,&a,&b);
+  c=a+b;
+  printf("")
