73f91236f03828b76e0d100fbab5aba5bda49e82
GRajeshwar315 <94883446+GRajeshwar315@users.noreply.github.com>
2022-01-04 15:10:40 +0000

    done

--- p4final.c
+++ p4final.c
@@ -1,1 +1,37 @@
-
+#include<stdio.h>
+int input()
+{
+  int s;
+  printf("enter the vaiue");
+  scanf("%d",&s);
+  return s;
+}
+int cmp(int a,int b,int c)
+{
+  if(a>b&&a>c)
+  {
+    return a;
+  }
+  else if (b>c&&b>c)
+  {
+    return b;
+  }
+  else
+  {
+    return c;
+  }
+}
+void output(int a,int b,int c,int s)
+{
+  printf("among %d,%d,%d\n%d is geratest",a,b,c,s);
+}
+int main()
+{
+  int r,p,t,s;
+  r=input();
+  t=input();
+  p=input();
+  s=cmp(r,t,p);
+  output(r,t,p,s);
+  return 0;
+}
